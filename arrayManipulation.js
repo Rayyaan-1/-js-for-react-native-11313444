f94532849cf3e5c42d51632f49cd594c3312270c
Rayyaan-1 <rabdul-hamid003@st.ug.edu.gh>
2024-05-19 17:46:04 +0000

    cmt 2

---
+++ arrayManipulation.js
@@ -0,0 +1,12 @@
+function processArray(arr) {
+    return arr.map(num => {
+        if (num % 2 === 0) {
+            return num * num;
+        } else {
+            return num * 3;
+        }
+    });
+}
+const numbers = [6, 7, 4, 3, 10];
+const processedNumbers = processArray(numbers);
+console.log(processedNumbers);
